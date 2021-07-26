3bc290328ef1150b36729e3f1f08c1e1215817d7
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2021-07-26 15:17:31 +0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/marketingApp";
+
+function App() {
+	return (
+		<div>
+			<h1>Hi</h1>
+			<hr />
+			<MarketingApp />
+		</div>
+	);
+}
+
+export default App;

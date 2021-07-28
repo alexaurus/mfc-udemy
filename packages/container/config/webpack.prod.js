37bac2a6a5529dd3ee95944a901934bff1afb58a
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2021-07-28 15:54:33 +0300

    Created test fo CI

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,6 +3,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
+console.log("Test");
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
